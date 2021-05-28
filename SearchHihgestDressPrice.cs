2ca75685098119fcb02a3aa52b044c93b2b290d0
JustinaStrimaityte <84452514+JustinaStrimaityte@users.noreply.github.com>
2021-05-28 16:20:39 +0300

    Add files via upload

---
+++ SearchHihgestDressPrice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace Baigiamasis_darbas.Test
+{
+    internal class SearchHihgestDressPrice
+    {
+        private IWebDriver driver;
+
+        public SearchHihgestDressPrice(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        internal void OrderByHighestPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void VerifyPrice(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
