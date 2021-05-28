250e3ef2e8f4915fbabdff2537e96ad787e96885
JustinaStrimaityte <84452514+JustinaStrimaityte@users.noreply.github.com>
2021-05-28 16:22:01 +0300

    Add files via upload

---
+++ BasePageClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace Baigiamasis_darbas.Page
+{
+   public class BasePageClass
+    {
+        protected static IWebDriver Driver;
+
+        public BasePageClass(IWebDriver webdriver)
+        {
+            Driver = webdriver;
+
+        }
+        public void Closebrowser()
+        {
+            Driver.Quit();
+        }
+    }
+}
