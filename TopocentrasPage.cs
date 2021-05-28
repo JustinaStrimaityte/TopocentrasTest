4a9b614d0c018d26e769609a0f10f7a09f900b17
JustinaStrimaityte <84452514+JustinaStrimaityte@users.noreply.github.com>
2021-05-28 16:22:40 +0300

    Add files via upload

---
+++ TopocentrasPage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace Baigiamasis_darbas.Page
+{
+   public class TopocentrasPage  : BasePageClass
+    {
+        private IWebElement searchInput => Driver.FindElement(By.Id("Sort-selectBox-2qH"));
+        private IWebElement searchIcon => Driver.FindElement(By.ClassName("Sort-selectBox-2qH"));
+        public TopocentrasPage(IWebDriver webdriver): base(webdriver) { }
+
+        public void SearchByText(string paspirtukas)
+        {
+            Driver.FindElement(By.ClassName("HeaderContent-searchInput-3Ks")).SendKeys(paspirtukas);
+            Driver.FindElement(By.ClassName("HeaderContent-searchInput-3Ks")).SendKeys(Keys.Return);
+
+        }
+
+
+
+
+        ////public void PressOnSearchbutton()
+        ////        {
+        ////            searchIcon.Click();
+        ////        }
+            }                                       
+        }  
+        
+
+        
+        
+
+        
+    
+
